b39ea37775b14f00661ccc4782e7e9cb7705061d
David OR <dev@orkapod.com>
2025-12-07 02:57:29 +0000

    b9e466d3-af0f-4486-9778-89a362063b0f

--- app/pages/dashboard.py
+++ app/pages/dashboard.py
@@ -50,8 +50,8 @@ def dashboard_page() -> rx.Component:
                             droppable_stage_column(stage=stage),
                             class_name=rx.cond(
                                 KanbanState.mobile_active_stage == stage.name,
-                                "block w-full md:w-auto md:block h-full flex-shrink-0",
-                                "hidden md:block md:w-auto h-full flex-shrink-0",
+                                "flex md:flex w-full md:w-auto h-full flex-shrink-0",
+                                "hidden md:flex md:w-auto h-full flex-shrink-0",
                             ),
                             key=stage.name,
                         ),
